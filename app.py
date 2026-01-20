dbb7108d35e81b3d0e02fd9ee05a1c90c5d0533d
milwalk <mil.walkowski@gmail.com>
2026-01-20 13:06:15 +0000

    formatting-change with black

--- app.py
+++ app.py
@@ -7,15 +7,13 @@ app = Flask(__name__)
 
 @app.route("/health-check", methods=["GET"])
 def health_check():
-    return jsonify({
-        "status": "ok"
-    }), 200
+    return jsonify({"status": "ok"}), 200
 
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
     message = os.getenv("SERVER_HELLO", "Hello from app.py")
-    return message, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    return message, 200, {"Content-Type": "text/plain; charset=utf-8"}
 
 
 if __name__ == "__main__":
