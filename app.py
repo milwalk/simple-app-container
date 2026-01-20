59fa8f57451d7743559d8b45cde0bbadf192f171
milwalk <mil.walkowski@gmail.com>
2026-01-20 12:56:30 +0000

    correction to quality-checks

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/health-check", methods=["GET"])
 def health_check():
     return jsonify({
         "status": "ok"
     }), 200
 
+
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
     message = os.getenv("SERVER_HELLO", "Hello from app.py")
     return message, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False)
